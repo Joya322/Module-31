992ea2cc735bbf36a6b60a07dd22701acccdb5ba
Joya322 <jannatferdoush1212@gmail.com>
2022-09-02 12:15:53 +0600

    filter done

---
+++ filter.js
@@ -0,0 +1,6 @@
+const numbers = [3, 5, 33, 55, 29, 17, 12, 4];
+const bigNums = numbers.filter(number => number > 20);
+// console.log(bigNums);
+
+const tiny = numbers.filter(number => number < 10);
+console.log(tiny);
