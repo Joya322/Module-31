bc56175872b0ee0250a718d8b389db62afb9bf91
Joya322 <jannatferdoush1212@gmail.com>
2022-09-25 20:45:03 +0600

    problem7 solved

---
+++ problem7.js
@@ -0,0 +1,27 @@
+/* 
+let data = {
+    location: [
+        {
+            latitude: '34.5, 37.8',
+            longitude: '98.77, 58.7',
+            city: 'Hyderabad',
+            country: 'India',
+        }
+    ]
+};
+
+Console the value of city */
+
+let data = {
+    location: [
+        {
+            latitude: '34.5, 37.8',
+            longitude: '98.77, 58.7',
+            city: 'Hyderabad',
+            country: 'India',
+        }
+    ]
+};
+
+const city = data.location[0].city;
+console.log(city);
