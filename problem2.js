74cc71aeee694e0a5525fc26c8df16e51abc6d8e
Joya322 <jannatferdoush1212@gmail.com>
2022-09-25 19:49:38 +0600

    problem2 solved

---
+++ problem2.js
@@ -0,0 +1,7 @@
+/* You are given an array say: [33, 50, 79, 78, 90, 101, 30 ]. Now
+return/get all the elements which are divisible by 10 using
+array.filter() method. */
+
+const array = [33, 50, 79, 78, 90, 101, 30];
+const mulOfTen = array.filter(num => num % 10 == 0);
+console.log(mulOfTen);
