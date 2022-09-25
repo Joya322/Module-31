ed2d0f75e302625d480fb84ff7fa573e7e888a0e
Joya322 <jannatferdoush1212@gmail.com>
2022-09-25 20:37:39 +0600

    problem6 solved

---
+++ problem6.js
@@ -0,0 +1,16 @@
+/* 
+const student = {
+    name: 'Fredie',
+    age: 26,
+}
+
+Console the value of age 
+
+*/
+
+const student = {
+    name: 'Fredie',
+    age: 26,
+}
+
+console.log(student.age);
