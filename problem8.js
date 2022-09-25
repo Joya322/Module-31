2ecb30a88f444673930619282da834030c8916de
Joya322 <jannatferdoush1212@gmail.com>
2022-09-25 20:55:13 +0600

    problem8 solved

---
+++ problem8.js
@@ -0,0 +1,75 @@
+/*
+const user = {
+  "id": 1,
+  "name": "Leanne Graham",
+  "username": "Bret",
+  "email": "Sincere@april.biz",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+      "lat": "-37.3159",
+      "lng": "81.1496"
+    }
+  },
+  "phone": "1-770-736-8031 x56442",
+  "website": "hildegard.org",
+  "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+  }
+}
+
+● Console the value of email
+● Console the value of address
+● Console the value of city
+● Console the value of lat
+● Console the value of company
+name */
+
+const user = {
+  id: 1,
+  name: "Leanne Graham",
+  username: "Bret",
+  email: "Sincere@april.biz",
+  address: {
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998-3874",
+    geo: {
+      lat: "-37.3159",
+      lng: "81.1496",
+    },
+  },
+  phone: "1-770-736-8031 x56442",
+  website: "hildegard.org",
+  company: {
+    name: "Romaguera-Crona",
+    catchPhrase: "Multi-layered client-server neural-net",
+    bs: "harness real-time e-markets",
+  },
+};
+
+//Console the value of email
+const email = user.email;
+console.log(email);
+
+//Console the value of address
+const address = user.address;
+console.log(address);
+
+//Console the value of city
+const city = address.city;
+console.log(city);
+
+//Console the value of lat
+const lat = address.geo.lat;
+console.log(lat);
+
+//Console the value of company name
+const companyName = user.company.name;
+console.log(companyName);
